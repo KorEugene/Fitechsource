6347cc4951a04b4510c64e2d1d422223f1f5d703
ekorch <johnkorch@yandex.ru>
2019-02-21 00:29:08 +0300

    corrections for review

--- src/main/java/com/fitechsource/test/ParallelTest.java
+++ src/main/java/com/fitechsource/test/ParallelTest.java
@@ -4,62 +4,66 @@ import java.util.*;
 
 /**
  * Should be improved to reduce calculation time.
- *
+ * <p>
  * 1. Change this file or create new one using parallel calculation mode.
  * 2. Do not use `executors`, only plain threads  (max threads count which should be created for calculations is com.fitechsource.test.TestConsts#MAX_THREADS)
  * 3. Try to provide simple solution, do not implement frameworks.
  * 4. Don't forget that calculation method can throw exception, process it right way.
- *   (Stop calculation process and print error message. Ignore already calculated intermediate results, user doesn't need it.)
- *
- *   Please attach code files to email - skhisamov@fitechsource.com
+ * (Stop calculation process and print error message. Ignore already calculated intermediate results, user doesn't need it.)
+ * <p>
+ * Please attach code files to email - skhisamov@fitechsource.com
  */
 
 public class ParallelTest {
-    public static void main(String[] args) throws InterruptedException {
-        Set<Double> res = Collections.synchronizedSet(new HashSet<>());
-        List<Thread> threadList = new ArrayList<>();
-        int index = 0;
-
-        for (int i = 0; i < TestConsts.MAX_THREADS; i++) {
-            Thread thread = new CalculatingThread(res, createListOfValuesInRange(TestConsts.N, TestConsts.MAX_THREADS, i));
-            threadList.add(thread);
-            thread.start();
-        }
-
-        for (Thread thread : threadList) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new CalculationException("Error during calculation waiting", e);
-            }
-        }
-
+    public static void main(String[] args) {
+        Set<Double> res = calculateAsynchronously();
         System.out.println(res);
     }
 
-    public static List<Integer> createListOfValuesInRange(int countOfIterations, int countOfThreads, int startPosition) {
-        List<Integer> listOfValues = new ArrayList<>();
+    private static Set<Double> calculateAsynchronously() {
+        Set<Double> res = Collections.synchronizedSet(new HashSet<>());
+        List<Thread> threads = new ArrayList<>();
 
-        for (int i = startPosition; i < countOfIterations; i += countOfThreads) {
-            listOfValues.add(i);
+        for (int i = 0; i < TestConsts.MAX_THREADS; i++) {
+            Thread thread = new CalculatingThread(res, createSequenceForCalculating(i));
+            threads.add(thread);
+            thread.start();
         }
-        return listOfValues;
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new CalculationException("Error during calculation waiting", e);
+            }
+        }
+        return res;
+    }
+
+    private static List<Integer> createSequenceForCalculating(int startPosition) {
+        List<Integer> integers = new ArrayList<>();
+
+        for (int i = startPosition; i < TestConsts.N; i += TestConsts.MAX_THREADS) {
+            integers.add(i);
+        }
+        return integers;
     }
 }
 
 class CalculatingThread extends Thread {
 
     private Set<Double> res;
-    private List<Integer> listOfValues;
+    private List<Integer> values;
 
-    CalculatingThread(Set<Double> res, List<Integer> listOfValues) {
+    CalculatingThread(Set<Double> res, List<Integer> values) {
         this.res = res;
-        this.listOfValues = listOfValues;
+        this.values = values;
     }
 
     @Override
     public void run() {
-        for (Integer value : listOfValues) {
+        for (Integer value : values) {
             try {
                 res.addAll(TestCalc.calculate(value));
             } catch (TestException e) {
@@ -82,7 +86,6 @@ class CalculationException extends RuntimeException {
     public CalculationException(String message, Throwable cause) {
         super(message, cause);
     }
-
 
 }
 
--- src/main/java/com/fitechsource/test/Test.java
+++ src/main/java/com/fitechsource/test/Test.java
@@ -4,18 +4,20 @@ import java.util.*;
 
 /**
  * Should be improved to reduce calculation time.
- *
+ * <p>
  * 1. Change this file or create new one using parallel calculation mode.
  * 2. Do not use `executors`, only plain threads  (max threads count which should be created for calculations is com.fitechsource.test.TestConsts#MAX_THREADS)
  * 3. Try to provide simple solution, do not implement frameworks.
- * 4. Don't forget that calculation method can throw exception, process it right way. 
- *   (Stop calculation process and print error message. Ignore already calculated intermediate results, user doesn't need it.)
- *   
- *   Please attach code files to email - skhisamov@fitechsource.com
+ * 4. Don't forget that calculation method can throw exception, process it right way.
+ * (Stop calculation process and print error message. Ignore already calculated intermediate results, user doesn't need it.)
+ * <p>
+ * Please attach code files to email - skhisamov@fitechsource.com
  */
 
 public class Test {
     public static void main(String[] args) throws TestException {
+        long startTime = System.currentTimeMillis();
+
         Set<Double> res = new HashSet<>();
 
         for (int i = 0; i < TestConsts.N; i++) {
@@ -23,5 +25,8 @@ public class Test {
         }
 
         System.out.println(res);
+
+        long time = System.currentTimeMillis() - startTime;
+        System.out.println("the work was done in " + time + " milliseconds");
     }
 }
